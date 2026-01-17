d5d73394b97264862554f3df0ff04b63b2640b46
אחד שנים <3428yp3qjp@gmail.com>
2026-01-17 21:34:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,19 @@ if url:
     cookie_file = "cookies.txt" # שם הקובץ שהעלית לגיטהאב
 
     ydl_opts = {
-        'format': 'best',
+        # מחפש קובץ MP4 מוכן כדי לא להסתבך עם חיבורים
+        'format': 'best[ext=mp4]/best', 
         'outtmpl': video_filename,
-        'quiet': False,
+        'quiet': False, # שיניתי ל-False כדי שתוכל לראות לוגים בשרת אם זה נכשל
         'nocheckcertificate': True,
         'cookiefile': cookie_file,
-        # הגדרה שאומרת ל-yt-dlp להתחזות לאפליקציית אנדרואיד
-        'extractor_args': {
-            'youtube': {
-                'player_client': ['android'],
-            }
-        },
+        # הגדרות לעקיפת החידה של יוטיוב
+        'extract_flat': False,
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+        }
     }
 
     try:
